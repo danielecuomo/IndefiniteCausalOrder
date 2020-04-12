7cf169b385b01d7de917389ff5141de9abfea336
Daniele Cuomo <daniele.cuomo@unina.it>
2020-04-12 19:36:25 +0200

    Switch builder prototype implemented
    The SwitchBuilderPrototype is a prototype for instancing a quantum switch by using Gate objects. Nevertheless, framework doesn't provide information about a proper use of them.

---
+++ SwitchBuilderPrototype.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Create a parametrized gate implementing a quantum switch.
+#input D, E are Gate objects 
+#qubit 0 is the target.
+#qubit 1 is an ancilla gate, therefore it will be reset to state |+>.
+
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.quantum_info import Operator
+
+def OneQubitSwitchGate(D, E):
+
+    q = QuantumRegister(1, name='q')
+    a = QuantumRegister(1, name='a')
+    switch = QuantumCircuit(q, a, name='$\mathcal{S}$')
+    
+    Dmat = D.to_matrix()
+    cD = Operator([[Dmat[0][0], Dmat[0][1], 0, 0],
+                   [Dmat[1][0], Dmat[1][1], 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+
+    switch.reset(1)
+    switch.h(1)
+    switch.x(1)
+    switch.unitary(cD, [1, 0], 'D')
+    switch.x(1)
+    switch.barrier()
+    switch.append(E, 0)
+    switch.unitary(cD, [1, 0], 'D')
+
+    return switch.to_instruction()
